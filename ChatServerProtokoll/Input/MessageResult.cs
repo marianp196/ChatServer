3b3c87b13b5a71aa9177e7dfd79050b7d886f45d
marianp196 <marianpatzke@gmail.com>
2018-12-22 14:53:11 +0100

    MessageResult hinzugefügt

---
+++ ChatServerProtokoll/Input/MessageResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatserverProtokoll.Input
+{
+    public class MessageResult
+    {
+        public EState State { get; set; }
+        public Guid? ClientID { get; set; }
+        public Guid? ServerID { get; set; }
+
+        public IEnumerable<Error> Errors { get; set; }
+    }
+
+    public enum EState
+    {
+        Success,
+        Error,
+        Warning
+    }
+
+    public class Error
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+    }
+}
